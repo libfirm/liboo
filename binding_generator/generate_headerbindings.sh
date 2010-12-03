7f58d5f73c9910ae2e2480bccfda5885f6cdefe0
Matthias Braun <matze@braunis.de>
2010-12-03 13:55:49 +0100

    update java binding for new interface

---
+++ binding_generator/generate_headerbindings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# You need a new version cparser (with jna backend) and firm headers
+
+. config
+
+for i in oo mangle; do
+	RES="../src/firm/bindings/binding_$i.java"
+	TMP="/tmp/tmp.java"
+	TMP2="/tmp/tmp2.java"
+	OO_INC="../include"
+	echo " * Creating $RES"
+	CMD="cparser --print-jna --jna-libname oo -I${FIRM_INC} -I${OO_INC} ${OO_INC}/liboo/$i.h --jna-limit ${OO_INC}/liboo/$i.h"
+	echo "$CMD"
+	$CMD > $TMP || exit $?
+	sed -e "s/class binding/class binding_$i/g" -i $TMP
+	echo "package firm.bindings;" > $TMP2
+	echo "" >> $TMP2
+	cat $TMP2 $TMP > $RES
+done
+
---
+++ src/firm/OO.java
@@ -0,0 +1,85 @@
+package firm;
+
+import firm.bindings.binding_mangle;
+import firm.bindings.binding_oo;
+
+/**
+ * Object-Orientation helper library API
+ */
+public final class OO {
+	private static boolean initialized;
+
+	private OO() {
+	}
+
+	/**
+	 * initializes OO library. Must be called once before using any other
+	 * functions from this class.
+	 */
+	public static void init() {
+		assert initialized == false; /* we may only use 1 OO object at once... */
+		binding_oo.oo_init();
+	}
+
+	/**
+	 * deinitializes OO library. This frees some allocated resources
+	 */
+	public static void deinit() {
+		binding_oo.oo_deinit();
+		initialized = false;
+	}
+
+	/**
+	 * Lower Object-Oriented constructs into low-level stuff
+	 */
+	public static void lowerProgram() {
+		binding_oo.lower_oo();
+	}
+
+	/**
+	 * lets you configure which compound-types need a vtable
+	 */
+	public static void setClassNeedsVTable(CompoundType type, boolean needsVTable) {
+		binding_oo.set_class_needs_vtable(type.ptr, needsVTable);
+	}
+
+	/**
+	 * lets you configure which methods should be included in the vtable
+	 */
+	public static void setMethodIncludeInVTable(Entity entity, boolean includeInVTable) {
+		binding_oo.set_method_include_in_vtable(entity.ptr, includeInVTable);
+	}
+
+	/**
+	 * lets you mark a method as abstract
+	 */
+	public static void setMethodAbstract(Entity entity, boolean isAbstract) {
+		binding_oo.set_method_is_abstract(entity.ptr, isAbstract);
+	}
+
+	/**
+	 * lets you specify the binding mode of a method
+	 */
+	public static void setEntityBinding(Entity entity, binding_oo.ddispatch_binding binding) {
+		binding_oo.set_entity_binding(entity.ptr, binding.val);
+	}
+
+	public static void setClassVTableEntity(ClassType classType, Entity entity) {
+		binding_oo.set_class_vptr_entity(classType.ptr, entity.ptr);
+	}
+
+	/**
+	 * define names for primitive names for the name mangling
+	 */
+	public static void setPrimitiveTypeName(Type type, String name) {
+		binding_mangle.mangle_set_primitive_type_name(type.ptr, name);
+	}
+
+	/**
+	 * define method name substitions for name mangling
+	 * (typically used for operator X methods)
+	 */
+	public static void addNameSubstitution(String name, String mangled) {
+		binding_mangle.mangle_add_name_substitution(name, mangled);
+	}
+}
