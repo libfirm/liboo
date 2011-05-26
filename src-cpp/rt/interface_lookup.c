6b3ebf0d40e053851648ba0a2ef813931c9c86e3
Julian Oppermann <julian.oppermann@student.kit.edu>
2011-05-26 16:21:13 +0200

    More useful interface lookup panic.

--- src-cpp/rt/interface_lookup.c
+++ src-cpp/rt/interface_lookup.c
@@ -18,5 +18,5 @@ void *oo_rt_lookup_interface_method(class_info_t *klass, string_const_t *method_
 	if (klass->superclass)
 		return oo_rt_lookup_interface_method(klass->superclass, method_name);
 
-	panic("Interface lookup failed!");
+	panic("Interface lookup for %s in %s failed", get_string_const_chars(method_name), get_string_const_chars(klass->name));
 }
