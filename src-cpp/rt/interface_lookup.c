87e8762b41208f2a7036680f1ab16417eb9b6421
Matthias Braun <matthias.braun@kit.edu>
2012-11-23 12:54:52 +0100

    improve interface lookup
    avoid recursive calls, in case of error display the type where the
    search started (not where it ended).

--- src-cpp/rt/interface_lookup.c
+++ src-cpp/rt/interface_lookup.c
@@ -3,17 +3,20 @@
 #include "../adt/error.h"
 #include "rt.h"
 
-void *oo_rt_lookup_interface_method(const class_info_t *klass, const string_const_t *method_name)
+void *oo_rt_lookup_interface_method(const class_info_t *klass,
+                                    const string_const_t *method_name)
 {
-	for (uint32_t i = 0; i < klass->n_methods; i++) {
-		if (string_const_equals(klass->methods[i].name, method_name)) {
-			return klass->methods[i].funcptr;
+	const class_info_t *k = klass;
+	do {
+		for (uint32_t i = 0; i < k->n_methods; i++) {
+			const method_info_t *method = &k->methods[i];
+			if (string_const_equals(method->name, method_name)) {
+				return method->funcptr;
+			}
 		}
-	}
-
-	// method is not declared in the current class, try the superclass
-	if (klass->superclass)
-		return oo_rt_lookup_interface_method(klass->superclass, method_name);
+		// not found, try the superclass
+		k = k->superclass;
+	} while (k != NULL);
 
 	panic("Interface lookup for %s in %s failed", get_string_const_chars(method_name), get_string_const_chars(klass->name));
 }
