7a90d63855f75e54981c2270746a6d4409c82b54
Manuel Mohr <manuel.mohr@kit.edu>
2013-06-13 14:24:58 +0200

    Adapt debug info to newer libfirm.

--- src-cpp/debuginfo.c
+++ src-cpp/debuginfo.c
@@ -12,20 +12,22 @@
 typedef struct debuginfo_t {
 	ident *filename;
 	uint32_t line;
-	//uint16_t column;
-	//uint16_t length;
+	uint16_t column;
+	uint16_t length;
 } debuginfo_t;
 
 static struct obstack obst;
 
-static const char *dbg_retrieve(const dbg_info *dbg, unsigned *line)
+static src_loc_t dbg_retrieve(const dbg_info *dbg)
 {
 	const debuginfo_t *info = (const debuginfo_t*) dbg;
+	src_loc_t loc = { NULL, 0, 0 };
 	if (info == NULL)
-		return NULL;
-	if (line != NULL)
-		*line = info->line;
-	return get_id_str(info->filename);
+		return loc;
+	loc.file   = get_id_str(info->filename);
+	loc.line   = info->line;
+	loc.column = info->column;
+	return loc;
 }
 
 void debuginfo_init(void)
@@ -45,9 +47,7 @@ const dbg_info *create_debuginfo(ident *filename, unsigned line,
 	debuginfo_t *info = OALLOC(&obst, debuginfo_t);
 	info->filename = filename;
 	info->line     = (uint32_t) line;
-	(void) column;
-	(void) length;
-	//info->column   = (uint16_t) column;
-	//info->length   = (uint16_t) length;
+	info->column   = (uint16_t) column;
+	info->length   = (uint16_t) length;
 	return (const dbg_info*) info;
 }
