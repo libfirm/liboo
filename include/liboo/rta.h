9c33df7aff9503e833cab0a3f0cd6ede6af176dd
Steffen Knoth <s.kno@web.de>
2014-02-02 19:01:04 +0100

    changed RTA API to just one function

--- include/liboo/rta.h
+++ include/liboo/rta.h
@@ -6,34 +6,13 @@
 
 #include <libfirm/firm.h>
 
-#include "../src-cpp/adt/cpset.h"
-#include "../src-cpp/adt/cpmap.h"
 
-
-/** run Rapid Type Analysis
- * It runs over a reduced callgraph and detects which classes and methods are actually used and computes reduced sets of potentially called targets for each dynamically linked call.
- * @param entry_points all (public) entry points to program code (as ir_entity*), RTA must know of _all_ definitely executed code parts (main, static sections, global contructors or all public functions if it's a library), @note It's important to give absolutely _all_ entry points because RTA builds on a closed world assumption. Otherwise the results can be incorrect and can lead to defective programs!!
- * @param used_classes give pointer to empty uninitialized set for receiving results, This is a set where all used classes are put (as ir_type*).
- * @param used_methods give pointer to empty uninitialized set for receiving results, This is a set where all used methods are put (as ir_entity*).
- * @param dyncall_targets give pointer to empty uninitialized map for receiving results, This is a map where call entities are mapped to their actually used potential call targets (ir_entity* -> {ir_entity*}). It's used to optimize dynamically linked calls if possible. (see also function rta_optimize_dyncalls)
+/** runs Rapid Type Analysis and then tries to devirtualize dynamically linked calls and discards unused classes and methods
+ * @note RTA must know of _all_ definitely executed code parts (main, static sections, global contructors or all public functions if it's a library)! It's important to give absolutely _all_ entry points because RTA builds on a closed world assumption. Otherwise the results can be incorrect and can lead to defective programs!! RTA also won't work with programs that dynamically load classes at run-time!
+ * @param n_entry_points number of entry points in array entry_points
+ * @param entry_points array of ir_entity*, give all entry points to program code
  */
-void rta_run(cpset_t *entry_points, cpset_t *used_classes, cpset_t *used_methods, cpmap_t *dyncall_targets);
-
-
-/** frees memory allocated for the results returned by function run_rta
- * @note does not free the memory of the sets and maps themselves, just their content allocated during RTA
- * @param used_classes as returned by run_rta
- * @param used_methods as returned by run_rta
- * @param dyncall_targets as returned by run_rta
- */
-void rta_dispose_results(cpset_t *used_classes, cpset_t *used_methods, cpmap_t *dyncall_targets);
-
-
-/** devirtualizes dyncalls if their target set contains only one entry
- * @param entry_points same as used with rta_run
- * @param dyncall_targets the result map returned from rta_run
- */
-void rta_optimize_dyncalls(cpset_t *entry_points, cpmap_t *dyncall_targets);
+void rta_optimization(size_t n_entry_points, ir_entity** entry_points);
 
 
 #endif
