70f0dec565156385416fc2e40463b030cbe0ed82
Steffen Knoth <s.kno@web.de>
2014-04-02 03:07:57 +0200

    added support for initial live classes and methods to better support some necessary native methods

--- include/liboo/rta.h
+++ include/liboo/rta.h
@@ -21,10 +21,11 @@
  * @note RTA must know of _all_ definitely executed code parts (main, static sections, global contructors or all nonprivate functions if it's a library)! It's important to give absolutely _all_ entry points because RTA builds on a closed world assumption. Otherwise the results can be incorrect and can lead to defective programs!!
  * @note RTA also won't work with programs that dynamically load classes at runtime! It can lead to defective programs!!
  * @note RTA might also produce incorrect programs if there is some Java Reflections shenanigans in the code (internal or external), especially using java.lang.Class.newInstance()
- * @param n_entry_points number of entry points in array entry_points
- * @param entry_points array of ir_entity*, give all entry points to program code
+ * @note Give classes that are instantiated in native methods of a nonexternal standard library or runtime as initial_live_classes, give methods called in these native methods as additional entry points. If something is missing, RTA could produce incorrect programs! This also means that native methods in the program that do arbitrary things are not supported.
+ * @param entry_points NULL-terminated array of method entities, give all entry points to program code, may _not_ be NULL and must contain at least one method entity, also all entry points should have a graph
+ * @param initial_live_classes NULL-terminated array of classes that should always be considered live, may be NULL
  */
-void rta_optimization(size_t n_entry_points, ir_entity** entry_points);
+void rta_optimization(ir_entity** entry_points, ir_type **initial_live_classes);
 
 
 #endif
